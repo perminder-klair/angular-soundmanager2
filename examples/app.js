c442791cfb4e163fab68fe4e7efbaba39cdcc8d8
parminder <perminder.klair@gmail.com>
2014-07-11 20:18:44 +0100

    updated example

--- examples/app.js
+++ examples/app.js
@@ -1,14 +1,32 @@
-var myApp = angular.module('angularSoundManager', []);
+angular.module('myApp', ['angularSoundManager'])
 
-myApp.filter('humanTime', function () {
-    return function (input) {
-        function pad(d) {
-            return (d < 10) ? '0' + d.toString() : d.toString();
-        }
+    .controller('MainCtrl', ['$scope', 'player', function ($scope) {
+        $scope.songs = [
+            {
+                id: 'one',
+                title: 'Leh',
+                url: 'http://cdn11.imp3songs.com/files/superwoman/leh-feat-humble-the-poet/128/01-leh-feat-humble-the-poet.mp3'
+            },
+            {
+                id: 'two',
+                title: 'Delete',
+                url: 'http://cdn11.imp3songs.com/files/gagan-kokri/delete/128/01-delete.mp3'
+            },
+            {
+                id: 'three',
+                title: 'Police (Feat. Deep Jandu)',
+                url: 'http://cdn11.imp3songs.com/files/blizzy/police-feat-deep-jandu/128/01-police-feat-deep-jandu.mp3'
+            },
+            {
+                id: 'four',
+                title: 'Life Of Villager',
+                url: 'http://cdn11.imp3songs.com/files/aman-dhillon/life-of-villager-duniyadari/128/01-life-of-villager-duniyadari.mp3'
+            },
+            {
+                id: 'five',
+                title: 'Dil Cherdi',
+                url: 'http://cdn11.imp3songs.com/files/jay-status/dil-cherdi/128/01-dil-cherdi.mp3'
+            }
+        ];
 
-        var min = (input / 1000 / 60) << 0,
-            sec = Math.round((input / 1000) % 60);
-
-        return pad(min) + ':' + pad(sec);
-    };
-});
+    }]);
