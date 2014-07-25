c1114ff937aa3ecf53e02946e6f965f20325cd83
perminder-klair <klair.di.sardari@gmail.com>
2014-07-25 15:19:42 +0000

    include artists in example

--- examples/app.js
+++ examples/app.js
@@ -5,26 +5,31 @@ angular.module('myApp', ['angularSoundManager'])
             {
                 id: 'one',
                 title: 'Rain',
+                artist: 'Drake',
                 url: 'http://www.schillmania.com/projects/soundmanager2/demo/_mp3/rain.mp3'
             },
             {
                 id: 'two',
                 title: 'Walking',
+                artist: 'Nicki Minaj',
                 url: 'http://www.schillmania.com/projects/soundmanager2/demo/_mp3/walking.mp3'
             },
             {
                 id: 'three',
                 title: 'Barrlping with Carl (featureblend.com)',
+                artist: 'Akon',
                 url: 'http://www.freshly-ground.com/misc/music/carl-3-barlp.mp3'
             },
             {
                 id: 'four',
                 title: 'Angry cow sound?',
+                artist: 'A Cow',
                 url: 'http://www.freshly-ground.com/data/audio/binaural/Mak.mp3'
             },
             {
                 id: 'five',
                 title: 'Things that open, close and roll',
+                artist: 'Someone',
                 url: 'http://www.freshly-ground.com/data/audio/binaural/Things%20that%20open,%20close%20and%20roll.mp3'
             }
         ];
