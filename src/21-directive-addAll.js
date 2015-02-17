5f83f52adcc97e127ed0132de5af76ed0c750fdc
Parminder Klair <perminder.klair@gmail.com>
2015-02-17 17:25:24 +0000

    added addAll directive

---
+++ src/21-directive-addAll.js
@@ -0,0 +1,22 @@
+ngSoundManager.directive('addAll', ['angularPlayer', '$log',
+    function(angularPlayer, $log) {
+        return {
+            restrict: "EA",
+            scope: {
+                songs: '=playAll'
+            },
+            link: function(scope, element, attrs) {
+                element.bind('click', function(event) {
+                    //first clear the playlist
+                    angularPlayer.clearPlaylist(function(data) {
+                        $log.debug('cleared, ok now add to playlist');
+                        //add songs to playlist
+                        for(var i = 0; i < scope.songs.length; i++) {
+                            angularPlayer.addTrack(scope.songs[i]);
+                        }
+                    });
+                });
+            }
+        };
+    }
+]);
