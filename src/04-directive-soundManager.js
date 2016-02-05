716c3d5066d9b28a3e31c97a1b1424cfbab66675
Ondřej Dušek <ondrej.dusek@pasante.cz>
2016-02-05 12:04:39 +0100

    $apply in progress, perminder-klair/angular-soundmanager2#42

--- src/04-directive-soundManager.js
+++ src/04-directive-soundManager.js
@@ -6,34 +6,34 @@ ngSoundManager.directive('soundManager', ['$filter', 'angularPlayer',
                 //init and load sound manager 2
                 angularPlayer.init();
                 scope.$on('track:progress', function(event, data) {
-                    scope.$apply(function() {
+                    $timeout(function() {
                         scope.progress = data;
                     });
                 });
                 scope.$on('track:id', function(event, data) {
-                    scope.$apply(function() {
+                    $timeout(function() {
                         scope.currentPlaying = angularPlayer.currentTrackData();
                     });
                 });
                 scope.$on('currentTrack:position', function(event, data) {
-                    scope.$apply(function() {
+                    $timeout(function() {
                         scope.currentPostion = $filter('humanTime')(data);
                     });
                 });
                 scope.$on('currentTrack:duration', function(event, data) {
-                    scope.$apply(function() {
+                    $timeout(function() {
                         scope.currentDuration = $filter('humanTime')(data);
                     });
                 });
                 scope.isPlaying = false;
                 scope.$on('music:isPlaying', function(event, data) {
-                    scope.$apply(function() {
+                    $timeout(function() {
                         scope.isPlaying = data;
                     });
                 });
                 scope.playlist = angularPlayer.getPlaylist(); //on load
                 scope.$on('player:playlist', function(event, data) {
-                    scope.$apply(function() {
+                    $timeout(function() {
                         scope.playlist = data;
                     });
                 });
